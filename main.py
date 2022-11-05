61e648fcc72f79b26690fb0c3b1ddbcd21b3b194
matthew2344 <57953122+matthew2344@users.noreply.github.com>
2022-11-05 23:08:20 +0800

    awe

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
-from flask import Flask, jsonify
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from flask import Flask, Response, jsonify, request
+from PIL import Image
+from flask_cors import CORS, cross_origin
+from preprocess import preprocesses
+from classifier import training
+from binascii import a2b_base64
+from io import StringIO, BytesIO
+import json
+import cv2
+import numpy as np
+import facenet
+import detect_face
 import os
+import pickle
+import tensorflow.compat.v1 as tf
+import threading
+import base64
 
 app = Flask(__name__)
 
@@ -12,6 +30,8 @@ def index():
 @app.route('/test')
 def test():
    return jsonify({"Choo Choo": "Testing Flask app 🚅"})
+
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
